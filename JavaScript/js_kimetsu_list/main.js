990ffe76fe0aae1bc2c27a90fe8e0ed78e53838c
wsigma21 <wallstlbl@gmail.com>
2023-11-23 17:01:30 +0900

    add ラジオボタンの選択でfetch対象を変更

--- JavaScript/js_kimetsu_list/main.js
+++ JavaScript/js_kimetsu_list/main.js
@@ -1,10 +1,27 @@
 
 async function fetchFile(fileName) {
-  const response = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${fileName}`);
-  console.log(response);
-  const json = await response.json();
-  console.log(json);
+  const response = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${fileName}.json`);
+  const data = await response.json();
+  return data
 }
 
-const fileName = 'kisatsutai.json';
-fetchFile(fileName);
+// init
+function init() {
+  // 初期表示
+  displayCharacters();
+
+  // ラジオボタンにイベントリスナをセット
+  const radioBtn = document.getElementById('radioBtn');
+  radioBtn.addEventListener('change', ()=> {
+    const fileName = radioBtn.category.value;
+    displayCharacters(fileName);
+  })
+}
+
+async function displayCharacters(fileName = 'all') {
+  console.log(fileName);
+  const data = await fetchFile(fileName);
+  console.log(data);
+}
+
+init();
