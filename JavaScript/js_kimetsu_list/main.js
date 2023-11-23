93f46b9f08d2f1cbce85b6d6e907fff1272bece8
wsigma21 <wallstlbl@gmail.com>
2023-11-23 17:58:46 +0900

    add データがなかった場合のエラー処理を追加

--- JavaScript/js_kimetsu_list/main.js
+++ JavaScript/js_kimetsu_list/main.js
@@ -1,13 +1,30 @@
+const messageArea = document.getElementById('message-area');
+const message = document.getElementById('message');
 
 async function fetchFile(fileName) {
   const response = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${fileName}.json`);
-  const data = await response.json();
-  return data
+  console.log("response=", response);
+  if (response.ok) {
+    const data = await response.json();
+    // const data = [];
+    if (!data.length) {
+      throw new NoDataError('no data found');
+    }
+    return data
+  }
+}
+
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NoDataError";
+  }
 }
 
 // init
 function init() {
   // 初期表示
+  hiddenMessage();
   displayCharacters();
 
   // ラジオボタンにイベントリスナをセット
@@ -19,9 +36,27 @@ function init() {
 }
 
 async function displayCharacters(fileName = 'all') {
-  console.log(fileName);
-  const data = await fetchFile(fileName);
-  console.log(data);
+  try {
+    console.log(fileName);
+    const data = await fetchFile(fileName);
+    console.log(data);
+  } catch(e) {
+    if (e.name === "NoDataError") {
+      const errorMessage = "データがありません。。別のキャラクターを選んでね！";
+      displayMessage(errorMessage)
+    }
+  } 
+}
+
+function displayMessage(errorMessage) {
+  messageArea.style.display = "block";
+  message.innerText = errorMessage;
+}
+
+function hiddenMessage() {
+  console.log("メッセージエリア非表示");
+  messageArea.style.display = "none";
+  message.innerText = "";
 }
 
 init();
