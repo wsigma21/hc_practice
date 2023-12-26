dd62de4b93819ac899726b4de28baa6906f3a08e
wsigma21 <wallstlbl@gmail.com>
2023-12-26 21:05:35 +0900

    feat 処理部分のみ一通り実装

--- React/additional_assignment2/src/App.tsx
+++ React/additional_assignment2/src/App.tsx
@@ -1,7 +1,46 @@
+import { useState, ChangeEvent } from "react";
+
+type TodoType = {
+  id: number,
+  title: string
+}
+
+let nextId = 0;
 
 function App() {
+  const [ todoText, setTodoText ] = useState<string>("");
+  const [ todos, setTodos ] = useState<TodoType[]>([]);
+
+  const onChangeTodoText = (event: ChangeEvent<HTMLInputElement>) => {
+    setTodoText(event.target.value);
+  }
+
+  const onAddTodo = () => {
+    if (todoText === "") return;
+    const newTodos = [...todos, { id : nextId++, title: todoText} ];
+    setTodos(newTodos);
+    setTodoText("");
+  }
+
+  const onDeleteTodo = (id:number) => {
+    const newTodos = todos.filter((t) => t.id !== id);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
+      <input type="text" width="20px" value={todoText} onChange={onChangeTodoText}></input>
+      <button onClick={onAddTodo}>追加</button>
+      { todos.length > 0 && (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.title}
+              <button onClick={() => onDeleteTodo(todo.id)}>削除</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
