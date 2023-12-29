bfe7ec2bb806a118c227a1a698f8b47ac64ad006
wsigma21 <wallstlbl@gmail.com>
2023-12-29 10:15:25 +0900

    refactor: TailWindでCSSを当てるよう変更

--- React/additional_assignment2/src/App.tsx
+++ React/additional_assignment2/src/App.tsx
@@ -30,81 +30,29 @@ function App() {
 
   return (
     <div className="App">
-      {/* <div className="flex items-center justify-center pt-20 w-screen h-full"> */}
-      {/* <div className="grid grid-cols-1 grid-rows-2 justify-items-center mt-10"> */}
-      <div style={containerStyle}>
-        {/* <div className="grid-item"> */}
-        <div style={inputAreaStyle}>
-          {/* <input type="text" className="h-100 border border-black-600 rounded-md" value={todoText} onChange={onChangeTodoText}></input> */}
-          <input type="text" style={inputStyle} value={todoText} onChange={onChangeTodoText}></input>
-          {/* <button className="bg-green-400 p-2 rounded-md text-white" onClick={onAddTodo}>追加</button> */}
-          <button style={addButtonStyle} onClick={onAddTodo}>追加</button>
+      <div className="w-3/12 mx-auto mt-10">
+        <div className="w-full mb-5">
+          <input type="text" className="w-9/12 py-1.5 px-2 border border-gray-300 rounded-md" value={todoText} onChange={onChangeTodoText}></input>
+          <button className="w-3/12 py-1.5 border border-green-500 rounded-md bg-green-500 text-white" onClick={onAddTodo}>追加</button>
         </div>
         
-        {/* <div className="grid-item"> */}
         <div>
           { todos.length > 0 && (
-            <>
+            <div>
               {todos.map((todo, index) => (
-                // <div key={todo.id} className="border w-border-red-500 rounded-md flex justify-between">
-                <div style={listAreaStyle} key={todo.id}>
-                  <span className="">{todo.title}</span>
-                  {/* <button className="bg-red-400 p-2 rounded-md text-white" onClick={() => onDeleteTodo(todo.id)}>削除</button> */}
-                  <button style={DeleteButtonStyle} onClick={() => onDeleteTodo(todo.id)}>削除</button>
+                <div className="group">
+                  <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
+                    <span className="w-9/12">{todo.title}</span>
+                    <button className="w-3/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white" onClick={() => onDeleteTodo(todo.id)}>削除</button>
+                  </div>
                 </div>
               ))}
-            </>
+            </div>
           )}
         </div>
       </div>
     </div>
   );
-}
-
-const containerStyle = {
-  width: "300px",
-  marginTop: "30px",
-  marginRight: "auto",
-  marginLeft: "auto",
-}
-
-const inputAreaStyle = {
-  width: "100%",
-  marginBottom: "20px",
-}
-
-const inputStyle = {
-  width: "80%",
-  border: "1px solid #c0c0c0",
-  borderRadius: "5px",
-  padding: "5px",
-}
-
-const addButtonStyle = {
-  width: "20%",
-  border: "1px solid green",
-  borderRadius: "5px",
-  backgroundColor: "green",
-  color: "white",
-  padding: "5px",
-}
-
-const listAreaStyle = {
-  border: "1px solid #c0c0c0",
-  borderRadius: "5px",
-  padding: "5px 10px",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-}
-
-const DeleteButtonStyle = {
-  width: "20%",
-  border: "1px solid red",
-  borderRadius: "5px",
-  backgroundColor: "red",
-  color: "white",
-  padding: "5px",
 }
 
 export default App;
