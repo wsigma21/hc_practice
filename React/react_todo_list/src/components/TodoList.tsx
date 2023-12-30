17674e4fd0852a4a3dc5f3242d073e01b78c879a
wsigma21 <wallstlbl@gmail.com>
2023-12-30 19:46:52 +0900

    refactor: 不要な条件文削除

--- React/react_todo_list/src/components/TodoList.tsx
+++ React/react_todo_list/src/components/TodoList.tsx
@@ -31,48 +31,46 @@ export const TodoList = () => {
   if (todos.length === 0 ) return <></>
   return (
     <>
-      { todos.length > 0 && (
-        <div className="group">
-          {todos.map((todo) => (
-            <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
-              <input type="checkbox" className="mr-2 border border-red-500" onChange={() => onChangeStatus(todo.id)} />
-              <input
-                type="text"
-                className="w-8/12 p-2 mr-2 border rounded-md disabled:bg-gray-100" 
-                value={todo.isEdit ? todo.editTitle : todo.title} 
-                disabled={!todo.isEdit} 
-                onChange={(event)=>{ onChageEditText(event, todo.id)}}
-              />
-              <button
-                className={ todo.isEdit ? saveButtonStyle : editButtonStyle }
-                onClick={()=> onEditTodo(todo.id)}
-              >{todo.isEdit ? "保存" : "編集"}</button>
-              <button
-                className="w-2/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500"
-                onClick={() => showDeleteModal(todo.id)}
-              >削除</button>
-            </div>
-          ))}
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Delete Modal"
-          >
-            <p className="mb-4">本当に削除してもよろしいですか？</p>
-            <div className="flex justify-end">
-              <button
-                className="w-24 p-2 text-sm border border-blue-500 rounded-md bg-blue-500 text-white"
-                onClick={onClickDeleteTodo}
-              >OK</button>
-              <button
-                className="w-24 p-2 ml-2 text-sm border border-gray-200 rounded-md text-blue-500"
-                onClick={closeModal}
-              >キャンセル</button>
-            </div>
-          </Modal>
-        </div>
-      )}
+      <div className="group">
+        {todos.map((todo) => (
+          <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
+            <input type="checkbox" className="mr-2 border border-red-500" onChange={() => onChangeStatus(todo.id)} />
+            <input
+              type="text"
+              className="w-8/12 p-2 mr-2 border rounded-md disabled:bg-gray-100" 
+              value={todo.isEdit ? todo.editTitle : todo.title} 
+              disabled={!todo.isEdit} 
+              onChange={(event)=>{ onChageEditText(event, todo.id)}}
+            />
+            <button
+              className={ todo.isEdit ? saveButtonStyle : editButtonStyle }
+              onClick={()=> onEditTodo(todo.id)}
+            >{todo.isEdit ? "保存" : "編集"}</button>
+            <button
+              className="w-2/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500"
+              onClick={() => showDeleteModal(todo.id)}
+            >削除</button>
+          </div>
+        ))}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Delete Modal"
+        >
+          <p className="mb-4">本当に削除してもよろしいですか？</p>
+          <div className="flex justify-end">
+            <button
+              className="w-24 p-2 text-sm border border-blue-500 rounded-md bg-blue-500 text-white"
+              onClick={onClickDeleteTodo}
+            >OK</button>
+            <button
+              className="w-24 p-2 ml-2 text-sm border border-gray-200 rounded-md text-blue-500"
+              onClick={closeModal}
+            >キャンセル</button>
+          </div>
+        </Modal>
+      </div>
     </>
   )
 }
