29c7ea5d446003375015685bd6d2d5a1a9715cce
wsigma21 <wallstlbl@gmail.com>
2023-12-30 14:46:14 +0900

    refactor:  不要なdivタグの削除をしてkeyが効くようにする

--- React/react_todo_list/src/components/TodoList.tsx
+++ React/react_todo_list/src/components/TodoList.tsx
@@ -28,16 +28,14 @@ export const TodoList = () => {
   return (
     <>
       { todos.length > 0 && (
-        <div>
+        <div className="group">
           {todos.map((todo) => (
-            <div className="group">
-              <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
-                <span className="w-9/12">{todo.title}</span>
-                <button
-                  className="w-3/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500"
-                  onClick={() => showDeleteModal(todo.id)}
-                >削除</button>
-              </div>
+            <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
+              <span className="w-9/12">{todo.title}</span>
+              <button
+                className="w-3/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500"
+                onClick={() => showDeleteModal(todo.id)}
+              >削除</button>
             </div>
           ))}
           <Modal
