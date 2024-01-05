913b35106d6bf88061229f61fcf54cd4b16cd051
wsigma21 <wallstlbl@gmail.com>
2024-01-05 22:13:19 +0900

    refactor: 不要な条件の削除

--- React/additional_assignment2/src/components/TodoList.tsx
+++ React/additional_assignment2/src/components/TodoList.tsx
@@ -6,19 +6,18 @@ export const TodoList = () => {
   const { todos } = useContext(TodoContext);
   const { onDeleteTodo } = useTodoList();
   if (todos.length === 0 ) return <></>
-  return (<>
-      { todos.length > 0 && (
-        <div>
-          {todos.map((todo) => (
-            <div className="group">
-              <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
-                <span className="w-9/12">{todo.title}</span>
-                <button className="w-3/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white" onClick={() => onDeleteTodo(todo.id)}>削除</button>
-              </div>
+  return (
+    <>
+      <div>
+        {todos.map((todo) => (
+          <div className="group">
+            <div key={todo.id} className="group-last:border-b w-full py-1.5 px-2 border-t border-r border-l border-gray-300 rounded-md flex justify-between items-center">
+              <span className="w-9/12">{todo.title}</span>
+              <button className="w-3/12 py-1.5 border border-red-500 rounded-md bg-red-500 text-white" onClick={() => onDeleteTodo(todo.id)}>削除</button>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
     </>
   )
 }
