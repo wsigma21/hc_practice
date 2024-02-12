f45f669edb33da40333ed19b3fd0ff5f6ac01eb3
wsigma21 <wallstlbl@gmail.com>
2024-02-12 19:28:31 +0900

    build: install react-modal

---
+++ TypeScript/ts-practice/src/components/organisms/CustomModal.tsx
@@ -0,0 +1,58 @@
+import { memo, FC } from "react";
+import Modal from 'react-modal';
+import { ModalType } from "../../types/modal" 
+
+type CustomModalType = {
+  modal: ModalType;
+  confirmText: string;
+  confirm: () => void;
+  cancel?: () => void;
+  children: React.ReactNode;
+}
+
+export const CustomModal: FC<CustomModalType> = memo(({modal, confirmText, confirm, cancel, children}) => {
+  const customStyles = {
+    content: {
+      top: '25%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      width: '500px',
+      height: '500px',
+      marginRight: '-50%',
+      borderRadius: '3%',
+      transform: 'translate(-50%, -50%)',
+    },
+  }
+
+  return (
+    <Modal
+      isOpen={modal.isOpen}
+      onRequestClose={cancel}
+      style={customStyles}
+      contentLabel="Delete Modal"
+    >
+      <p className="mb-4">{confirmText}</p>
+      {children}
+      <div className="flex justify-end">
+        <button
+          className="
+            w-24 p-2 
+            text-sm text-white
+            border border-blue-500 rounded-md 
+            bg-blue-500"
+          onClick={confirm}
+        >登録</button>
+        { cancel && (
+          <button
+            className="
+              w-24 p-2 ml-2 
+              text-sm text-blue-500
+              border border-gray-200 rounded-md"
+            onClick={cancel}
+          >キャンセル</button>)
+        }
+      </div>
+    </Modal>
+  )
+})
