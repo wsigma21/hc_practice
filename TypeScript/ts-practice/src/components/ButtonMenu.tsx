e2f4cc5231c512838d673474983421038817bef6
wsigma21 <wallstlbl@gmail.com>
2024-02-17 12:21:56 +0900

    feat: 現在の表示に合わせて表切り替えボタンの色を変更

--- TypeScript/ts-practice/src/components/ButtonMenu.tsx
+++ TypeScript/ts-practice/src/components/ButtonMenu.tsx
@@ -3,23 +3,25 @@ import { UserAttributeContext } from "../providers/UserAttributeProvider";
 import { UserAttributeType } from "../types/userAttribute"
 
 export const ButtonMenu = () => {
-  const { setUserAttribute } = useContext(UserAttributeContext);
+  const { userAttribute, setUserAttribute } = useContext(UserAttributeContext);
   const changeListType = (selectedType: UserAttributeType) => {
     setUserAttribute(selectedType);
   }
-  const buttonStyle = "w-2/12 py-1.5 border border-teal-300 rounded-sm bg-teal-500 text-white hover:bg-white hover:text-teal-500"
+  const buttonStyle = "w-2/12 py-1.5 border border-emerald-500 rounded-sm tracking-widest"
+  const unSelectedButtonStyle = `${buttonStyle} bg-emerald-500 text-white`
+  const selectedunSelectedButtonStyle = `${buttonStyle} bg-white text-emerald-500`
   return (
     <div className="mb-5 flex justify-center">
       <button
-        className={buttonStyle}
+        className={userAttribute !== "student" && userAttribute !== "mentor" ? selectedunSelectedButtonStyle : unSelectedButtonStyle}
         onClick={() => changeListType("allUser")}
       >全員</button>
       <button
-        className={buttonStyle}
+        className={userAttribute === "student" ? selectedunSelectedButtonStyle : unSelectedButtonStyle}
         onClick={() => changeListType("student")}
-      >生徒のみ</button>
+        >生徒のみ</button>
       <button
-        className={buttonStyle}
+        className={userAttribute === "mentor" ? selectedunSelectedButtonStyle : unSelectedButtonStyle}
         onClick={() => changeListType("mentor")}
       >メンターのみ</button>
     </div>
